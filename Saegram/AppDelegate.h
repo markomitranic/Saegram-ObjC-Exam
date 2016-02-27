279a6e76c55457b378dc619fe8af7239b29021f8
markomitranic <marko.mitranic@me.com>
2016-02-27 23:09:04 +0100

    Initial Commit

---
+++ Saegram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Saegram
+//
+//  Created by Marko Mitranić on 2/27/16.
+//  Copyright © 2016 Striking Spiders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Saegram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Saegram
+//
+//  Created by Marko Mitranić on 2/27/16.
+//  Copyright © 2016 Striking Spiders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
